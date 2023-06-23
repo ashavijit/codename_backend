a99f1c53ceeefc541055b0deaa5713360c189e3e
ashavijit <as2440@it.jgec.ac.in>
2023-06-23 23:36:47 +0530

    fix op

--- admin/admin.go
+++ admin/admin.go
@@ -1,16 +1,21 @@
 package admin
 
 import (
-	"context"
 	"codename_backend/database"
 	"codename_backend/models"
+	"context"
 
 	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/bson"
 )
 
+const (
+	DB_NAME        = "codename"
+	CollectionName = "users"
+)
+
 func GetALLUSERS(c *gin.Context) {
-	collection := database.GetCollection("codename") 
+	collection := database.GetCollection(CollectionName)
 	cursor, err := collection.Find(context.Background(), bson.M{})
 	if err != nil {
 		c.JSON(500, gin.H{
